daed38b8c0123b657ee02d9259d8c6de6a43a0ed
Arya Deep Singh <singharya9693@gmail.com>
2026-03-25 16:47:45 +0530

    Remove commented testing code from App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,7 +3,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { Toaster } from 'react-hot-toast';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './lib/queryClient';
-
+//just testing
 // Pages
 import LandingPage from "./pages/LandingPage";
 import { SignupPage } from "./pages/auth/SignupPage";
