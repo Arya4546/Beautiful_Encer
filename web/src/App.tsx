f254cd6be9548c7c5f518010eba19e9f8ee69fb0
Arya Deep Singh <singharya9693@gmail.com>
2026-03-25 16:49:56 +0530

    Update comments in App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,7 +4,7 @@ import { Toaster } from 'react-hot-toast';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from './lib/queryClient';
 //just testing
-// testing again
+// testing 
 // Pages
 import LandingPage from "./pages/LandingPage";
 import { SignupPage } from "./pages/auth/SignupPage";
